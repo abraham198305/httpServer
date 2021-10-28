8df2c01be3c69b7f0fae7254bb4004cd074d06f0
Abraham <abey.alexander@gmail.com>
2021-10-28 14:47:38 +0530

    initial

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/rs/cors"
+)
+
+func main() {
+	port := 7000
+	dirStatic := filepath.Join("..", "www")
+	str := strings.Split(os.Getenv("SESSION_MANAGER"), `/`)[1]
+	nameHost := str[:len(str)-2]
+	log.Println(nameHost)
+	startHttpServer(port, dirStatic, nameHost)
+}
+
+func startHttpServer(port int, dirStatic string, nameHost string) {
+	log.Println("Server started @ http://" + nameHost + ":" + strconv.Itoa(port))
+	mux := http.NewServeMux()
+	mux.Handle(`/`, http.FileServer(http.Dir(dirStatic)))
+	//mux.HandleFunc("/", handlerRoot)
+	handler := cors.Default().Handler(mux)
+	log.Fatal(http.ListenAndServe(`:`+strconv.Itoa(port), handler))
+}
+
+/*
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+	pageContent, err := ioutil.ReadFile(`../aavarthi/index.html`)
+	if err != nil {
+		w.Write([]byte("File read Error"))
+		return
+	}
+	w.Write([]byte(pageContent))
+}
+*/
